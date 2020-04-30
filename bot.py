e77f6c68b8c7d6f5f9cdfd384dc110189515d077
Fedee120 <63765822+Fedee120@users.noreply.github.com>
2020-04-30 17:19:05 -0300

    1.0.1

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from discord.ext import commands
 from numpy import random
 import youtube_dl
+TOKEN = ''
 
 bot = commands.Bot(command_prefix='/')
 
@@ -12,4 +13,4 @@ async def ping(ctx):
     await ctx.send('pong')
 pass
 
-bot.run('NzA1MjM3Nzk5NTkwNDI4NzYz.XqsPDg.GP7FOPjNzEGJSuKPKmAsFXiV7gI')
+bot.run(TOKEN)
